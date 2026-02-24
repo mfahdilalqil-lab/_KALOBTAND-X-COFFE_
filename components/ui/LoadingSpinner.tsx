c00399f7f5fbfd4803930bffbc3ed51df43e2e79
mfahdilalqil-lab <mfahdilalqil@gmail.com>
2026-02-24 16:04:39 +0700

    Create LoadingSpinner.tsx

---
+++ components/ui/LoadingSpinner.tsx
@@ -0,0 +1,7 @@
+export function LoadingSpinner() {
+  return (
+    <div className="flex justify-center py-6">
+      <div className="w-8 h-8 border-2 border-[#C76B4C] border-t-transparent rounded-full animate-spin"></div>
+    </div>
+  );
+}
