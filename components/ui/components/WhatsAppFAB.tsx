e0203bf18fecd0396ae33515b5fa135523bb7589
mfahdilalqil-lab <mfahdilalqil@gmail.com>
2026-03-01 12:09:45 +0700

    Create WhatsAppFAB.tsx

---
+++ components/ui/components/WhatsAppFAB.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+export function WhatsAppFAB() {
+  return (
+    <a
+      href="https://wa.me/6281234567890?text=Hi%20Kalobtand%20X%20Coffee,%20I%20want%20to%20book%20a%20table"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="fixed bottom-6 right-6 w-14 h-14 bg-[#25D366] rounded-full shadow-lg flex items-center justify-center hover:scale-105 transition-transform duration-300 z-50"
+      aria-label="Chat with us on WhatsApp"
+    >
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" strokeWidth="2">
+        <path d="M22 16.92v.01"></path>
+        <path d="M16 16.92v.01"></path>
+        <path d="M10 16.92v.01"></path>
+        <path d="M16 16.92a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0z"></path>
+        <path d="M10 16.92a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0z"></path>
+        <path d="M16 16.92a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0z"></path>
+        <path d="M10 16.92a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0z"></path>
+        <path d="M16 16.92a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0z"></path>
+        <path d="M10 16.92a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0z"></path>
+        <path d="M16 16.92a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0z"></path>
+        <path d="M10 16.92a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0z"></path>
+        <path d="M16 16.92a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0z"></path>
+        <path d="M10 16.92a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0z"></path>
+        <path d="M16 16.92a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0z"></path>
+        <path d="M10 16.92a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0z"></path>
+        <path d="M16 16.92a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7
